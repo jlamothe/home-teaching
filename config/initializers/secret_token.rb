709587f53bb210d24003e05eed9b9ac934ac2561
Jonathan Lamothe <jonathan@jlamothe.net>
2013-10-18 21:47:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeTeaching::Application.config.secret_key_base = 'f3148d7cfe9bf2f64e7edb567a49d6bb39f96eb985a694c49591e4921b067c4dd71b16beb2ec64028c61aba4880f08f452d1ff55574bc28131ab102ea160e439'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HomeTeaching::Application.config.session_store :cookie_store, key: '_home-teaching_session'
