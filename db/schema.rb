c00db3ff5a757549cb0fc4d338a3b784108daa94
Jonathan Lamothe <jonathan@jlamothe.net>
2013-10-30 13:16:59 -0400

    added and configured devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020002046) do
+ActiveRecord::Schema.define(version: 20131030171159) do
 
   create_table "companionships", force: true do |t|
     t.datetime "created_at"
@@ -25,12 +25,24 @@ ActiveRecord::Schema.define(version: 20131020002046) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name",             null: false
-    t.string   "email",            null: false
+    t.string   "name",                                null: false
+    t.string   "email",                               null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "companionship_id"
     t.integer  "family_id"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
