7f86091c831bd53c8e31aef4b5bdd69ec82886f9
Jonathan Lamothe <jonathan@jlamothe.net>
2013-11-05 21:13:26 -0500

    don't crash Email#user when user can't be found

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,6 +2,6 @@ class Email < ActiveRecord::Base
   validates :address, null: false, uniqueness: true
 
   def user
-    User.find_by_email self.address
+    User.where(email: self.address).first
   end
 end
